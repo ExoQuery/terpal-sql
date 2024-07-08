87fa1674c9f849556d67682cd7b33c1c4e1d908e
Alexander Ioffe <deusaquilus@gmail.com>
2024-07-08 03:30:17 -0400

    Republish for 1.9.21

--- build-logic/src/main/kotlin/conventions.gradle.kts
+++ build-logic/src/main/kotlin/conventions.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 group = "io.exoquery"
 // Everything inherits the version from here
-version = "1.9.22-0.3.0"
+version = "1.9.21-0.3.0"
 
 check("$version".isNotBlank() && version != "unspecified")
     { "invalid version $version" }
